ff315b759d632e6e2f31923ff3ce61bedf32e542
vvenante3 <vitor.venante@hotmail.com>
2023-10-24 20:47:25 -0300

    requisição de dados - OK

--- Projeto/script.js
+++ Projeto/script.js
@@ -6,6 +6,33 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active');
 
+const tempClient = {
+    //criação do JSON
+    nome: "Vitor",
+    email: "vitor.testecroud@hotmail.com",
+    celular: "4112348765",
+    cidade: "Curitiba"
+}
+
+    //CRUD - criar FUNÇÕES em criar(create), ler(read), atualizr(update) e deletar(delete)
+const createCliente = (client) => {
+    const db_client = JSON.parse(localStorage.getItem('db_client')); //Pega os dados (convertidos em objeto) que já tem no banco de dados e abaixo acrescenta mais dados sem substituir.
+    db_client.push (client);
+    localStorage.setItem("db_client", JSON.stringify(db_client)) //cria um "banco de dados(cliente)" e envia o valor de client
+                                //transforma o objeto em String
+}
+
+const camposValidos = () => {
+    return document.getElementById('form').reportValidity() // verifica se todas as regras forem compridas
+}
+
+    //interação
+const saveClient = () => {
+    if (camposValidos()) { //verifica se há campos vazios
+        console.log("cadastrando cliente");
+    }
+}
+
 
     //EVENTOS
 document.getElementById('cadastrarCliente')
@@ -13,3 +40,6 @@ document.getElementById('cadastrarCliente')
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal);
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient);
